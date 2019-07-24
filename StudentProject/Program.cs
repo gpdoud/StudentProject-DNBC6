7817f22665c0baedb8fcae8b26e427f43708b29f
Greg Doud <gpdoud@gmail.com>
2019-07-24 15:58:57 -0400

    Added methods

--- StudentProject/Program.cs
+++ StudentProject/Program.cs
@@ -3,20 +3,37 @@
 namespace StudentProject {
     class Program {
         static void Main(string[] args) {
+
             Student brailee = new Student();
-            brailee.FirstName = "Brailee";
-            brailee.IsPaid = true;
-            brailee.Birthdate = new DateTime(1990, 01, 01);
+            brailee.SetStatus("current");
+            brailee.SetStatus("Current");
+            brailee.SetStatus("CURRENT");
+            Console.WriteLine($"Brailee status is {brailee.Status}");
 
-            Student jesse = new Student();
-            jesse.FirstName = "Jess";
-            jesse.IsPaid = true;
-            jesse.Birthdate = new DateTime(1990, 1, 1);
+            //brailee.CreditScore = 2000;
+            brailee.SetCreditScore(9000);
+            brailee.SetCreditScore(90);
+            brailee.SetCreditScore(900);
+            Console.WriteLine($"Brailee credit score is {brailee.CreditScore}");
 
-            string name = jesse.FirstName;
+            #region morecode
+            //brailee.FirstName = "Brailee";
+            //brailee.IsPaid = true;
+            //brailee.Birthdate = new DateTime(1990, 01, 01);
+
+            //Student jesse = new Student("Jesse", "Kyle");
+            //Console.WriteLine($"Jess: {jesse.FirstName} {jesse.LastName}");
+            //jesse.SetStatus("past");
+            //jesse.IsPaid = true;
+            //jesse.Birthdate = new DateTime(1990, 1, 1);
+
+            //Console.WriteLine($"Brailee's Bootcamp is {brailee.Bootcamp}");
+            //Console.WriteLine($"Jess's Bootcamp is {jesse.Bootcamp}");
+            #endregion
         }
     }
     class Student {
+        public int Id { get; set; } // primary key
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Address { get; set; }
@@ -29,8 +46,35 @@ namespace StudentProject {
         public string Phone { get; set; }
         public bool IsPaid { get; set; } = false;
         public DateTime Birthdate { get; set; }
+        public int CreditScore { get; private set; }
 
+        public void SetCreditScore(int score) {
+            if(score >= 400 && score <= 900) {
+                this.CreditScore = score;
+            } else {
+                Console.WriteLine("Credit score must be 400 <= x <= 900.");
+            }
+        }
+
+        public void SetStatus(string status) {
+
+            if((status == "CURRENT") 
+                || (status == "PAST") 
+                || (status == "FUTURE")) {
+                this.Status = status;
+            } else {
+                Console.WriteLine("Status must be CURRENT, PAST, or FUTURE");
+            }
+            
+        }
+
+        public Student(string firstName, string lastName) {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Bootcamp = "Java";
+        }
         public Student() {
+            this.Bootcamp = ".Net";
         }
     }
 }
